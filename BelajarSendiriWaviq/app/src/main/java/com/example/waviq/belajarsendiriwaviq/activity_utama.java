c8b843e28d9cf17468d8d2852c1c62f15c524d88
waviq <waviq.subkhi@gmail.com>
2016-04-23 01:54:26 +0700

    layout

--- BelajarSendiriWaviq/app/src/main/java/com/example/waviq/belajarsendiriwaviq/activity_utama.java
+++ BelajarSendiriWaviq/app/src/main/java/com/example/waviq/belajarsendiriwaviq/activity_utama.java
@@ -2,9 +2,19 @@ package com.example.waviq.belajarsendiriwaviq;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activity_utama extends AppCompatActivity {
 
+    public void submit(View v){
+
+        EditText input = (EditText)findViewById(R.id.input);
+        TextView a = (TextView)findViewById(R.id.waviq);
+        a.setText(input.getText().toString());
+        input.getText().clear();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
